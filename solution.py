74fbe74b92c56b1f436e8e483cd904991b32d9d2
sorenh25 <jhssoren@hotmail.com>
2017-09-22 11:22:30 -0400

    Second part by Soren

--- solution.py
+++ solution.py
@@ -4,3 +4,9 @@ os.listdir(".")
 os.chdir('/Users/zoeloh/Desktop/Intro_Biocom_ND_319_Tutorial5')
 data = numpy.loadtxt(fname="test.dat",delimiter=" ")
 data
+
+data[:,0]==0
+
+data[;,0]>2
+
+data[data[:,0]>2,:]
