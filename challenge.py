4f9087db783d19bcec6394a43b8b813177e8265e
Zoe Loh <zloh@nd.edu>
2017-09-28 22:21:13 -0400

    part 1 and 2

--- challenge.py
+++ challenge.py
@@ -7,3 +7,22 @@ genderYears.drop_duplicates(['gender','yearsExperience'], keep='first', inplace=
 genderYears.sort_values(['gender','yearsExperience'], ascending=[True, True], inplace=True)
 genderYears.to_csv('uniqueGenderYears.txt', sep=' ')
 
+print("Highest earner:")
+print(wages.loc[wages['wage'].argmax(), ['gender', 'yearsExperience', 'wage']])
+
+print("Lowest earner:")
+print(wages.loc[wages['wage'].argmin(), ['gender', 'yearsExperience', 'wage']])
+
+collegeSalery = 0;
+highSchoolSalery = 0;
+collegeCount = 0;
+highSchoolCount = 0;
+
+for index in range(0, 3294 ,1):
+    if wages.loc[index, 'yearsSchool'] >= 16:
+        collegeCount += 1
+        collegeSalery += wages.loc[index, 'wage']
+    if wages.loc[index, 'yearsSchool'] <= 12:
+        highSchoolCount += 1
+        highSchoolSalery += wages.loc[index, 'wage']
+
